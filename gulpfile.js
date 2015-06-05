1b40af5f640fe2fd8cc38183de144d117627acbe
Ian Riley <ian.riley.915@gmail.com>
2015-06-05 14:41:22 -0700

    chore(build, config): Build and config now reflects current stage of tactical.
    gulp build compiles modules/ to dist/ using compiler options from tsconfig.json. package.json and tsd.json reflect current dependencies of the project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
 var cp = require('child_process');
+var gulp = require('gulp');
+var gulpTsc = require('gulp-typescript');
+
+// ==========
+// config
+
+var tsProject = gulpTsc.createProject('./tsconfig.json', {
+    typescript: require('typescript')
+  });
+
+
+// ==========
+// needed to execute local commands
 
 function exec(cmd, args, callback) {
   var proc = cp.spawn(cmd, args);
@@ -14,7 +26,16 @@ function exec(cmd, args, callback) {
   });
 }
 
-gulp.task('build', function(done) {
-  exec('tsc', ['-t', 'ES5', '-m', 'commonjs'], done);
-});
 
+// ==========
+// compile
+
+gulp.task('install.typings', function(done) {
+  exec('./node_modules/tsd/build/cli.js', ['reinstall'], done);
+})
+
+gulp.task('build', ['install.typings'], function(done) {
+  var tsResult = gulp.src('./modules/**/*.ts')
+      .pipe(gulpTsc(tsProject));
+  return tsResult.js.pipe(gulp.dest(tsProject.options.outDir));
+});
