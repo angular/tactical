9f809b2f97c23a6ac20215440e52c86bd8b1bbf6
Ian Riley <ian.riley.915@gmail.com>
2015-06-23 10:58:17 -0700

    chore(codegen): Remove codegen from modules.
    Codegen is no longer being used by tactical so the module is being removed.

--- modules/codegen/src/proto.ts
+++
@@ -1,8 +0,0 @@
-import {DotProto} from 'protobufjs';
-
-export function parseProtoFile(protoString: string): any {
-  var parser: any = new DotProto.Parser(protoString);
-  var ast: any = parser.parse();
-    
-  return ast;
-}  
