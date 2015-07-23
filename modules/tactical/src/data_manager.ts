83f453297349afd299827333dff9b965f9879a8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2015-07-23 14:42:20 -0700

    feat(data manager): Mutation support in the TacticalDataManager.
    Calling beginUpdate() returns an Updater, which provides a data value and also a method for committing back changes. Backend does not yet support mutations, so for now this is only local.

--- modules/tactical/src/data_manager.ts
+++ modules/tactical/src/data_manager.ts
@@ -4,18 +4,43 @@ import {Observable, Observer, Subject} from 'rx';
 import {Backend, VersionedObject} from './backend';
 import {serializeValue} from './json';
 import {Stream} from './stream';
-import {Record, Store} from './tactical_store';
+import {Record, Store, Version} from './tactical_store';
 
 /**
  * Manages subscriptions to data, and acts as an interface for the application.
  */
-export interface DataManager { request(key: Object): Observable<Object>; }
+export interface DataManager {
+  /**
+   * Request a particular key for read-only purposes.
+   */
+  request(key: Object): Observable<Object>;
+
+  /**
+   * Request to update a particular key. This will return at least one Updater,
+   * an object which references a specific version of the object. The consumer
+   * can then change this object and request for the changes to be committed.
+   */
+  beginUpdate(Key: Object): Observable<Updater>;
+}
+
+/**
+ * Provides a mechanism for updating a particular data object.
+ */
+export interface Updater {
+  value: Object;
+  commit(): Observable<boolean>;
+}
 
 export class TacticalDataManager implements DataManager {
   /**
    * Map of active keys to observables.
    */
-  private _stream: {[key: string]: Stream<Object>} = {};
+  private _stream: {[key: string]: Stream<Record>} = {};
+
+  /**
+   * Map of active updates.
+   */
+  private _updates: {[key: string]: Observer<Updater>[]} = {};
 
   /**
    * Create an instance of `TacticalDataManager`, backed by the given
@@ -31,9 +56,7 @@ export class TacticalDataManager implements DataManager {
   private _backendData(data: VersionedObject): void {
     var keyStr = serializeValue(data.key);
     this._store.push(data.key, data.data, data.version).subscribe();
-    if (this._stream.hasOwnProperty(keyStr)) {
-      this._stream[keyStr].send(data.data);
-    }
+    this._pushUpdate(keyStr, new Record(data.data, new Version(data.version)));
   }
 
   /**
@@ -44,8 +67,22 @@ export class TacticalDataManager implements DataManager {
    */
   request(key: Object): Observable<Object> {
     var keyStr = serializeValue(key);
+    return this._open(key, keyStr).map((rec: Record) => { return rec.value; });
+  }
+
+  commit(key: Object, value: Object, baseVersion: Version): Observable<boolean> {
+    return this._store.commit(key, value, baseVersion)
+        .map((record: Record): boolean => {
+          if (record != null) {
+            this._pushUpdate(serializeValue(key), record);
+          }
+          return (record != null);
+        });
+  }
+
+  _open(key: Object, keyStr: string): Observable<Record> {
     if (!this._stream.hasOwnProperty(keyStr)) {
-      this._stream[keyStr] = new Stream<Object>(() => { delete this._stream[keyStr]; });
+      this._stream[keyStr] = new Stream<Record>(() => { delete this._stream[keyStr]; });
       this._backend.request(key);
       this._requestFromStore(key);
     }
@@ -58,12 +95,29 @@ export class TacticalDataManager implements DataManager {
   private _requestFromStore(key: Object): void {
     var keyStr = serializeValue(key);
     this._store.fetch(key)
-        .filter(data => data != null)
-        .map(data => data.value)
-        .subscribe(data => {
-          if (this._stream.hasOwnProperty(keyStr)) {
-            this._stream[keyStr].send(data);
-          }
-        });
+        .filter(record => record != null)
+        .subscribe((record: Record) => { this._pushUpdate(keyStr, record); });
   }
+
+  private _pushUpdate(keyStr: string, record: Record) {
+    if (this._stream.hasOwnProperty(keyStr)) {
+      this._stream[keyStr].send(record);
+    }
+  }
+
+  beginUpdate(key: Object): Observable<Updater> {
+    var keyStr = serializeValue(key);
+    return this._open(key, keyStr)
+        .map((record: Record):
+             Updater => { return new TacticalUpdater(record.value, key, this, record.version); });
+  }
+}
+
+class TacticalUpdater implements Updater {
+  _valid: boolean = true;
+
+  constructor(public value: Object, private _key: Object, private _dm: TacticalDataManager,
+              private _version: Version) {}
+
+  commit(): Observable<boolean> { return this._dm.commit(this._key, this.value, this._version); }
 }
--- modules/tactical/test/data_manager_spec.ts
+++ modules/tactical/test/data_manager_spec.ts
@@ -76,4 +76,26 @@ describe('DataManager', () => {
           }
         });
   });
+  it('pushes mutations to the TacticalStore', (done) => {
+    var be = new FakeBackend();
+    var ts = new TacticalStore(InMemoryIdbFactory);
+    var dm = new TacticalDataManager(be, ts);
+    be.load(_versioned('v1', {key: true}, {foo: 'hello'}), false);
+    dm.beginUpdate({key: true})
+        .take(1)
+        .subscribe((updater) => {
+          expect(updater.value).to.deep.equal({foo: 'hello'});
+          updater.value['foo'] = 'goodbye';
+          updater.commit().subscribe((ok) => {
+            expect(ok).to.be.true;
+            ts.fetch({key: true})
+                .subscribe((data) => {
+                  expect(data.value).to.deep.equal({foo: 'goodbye'});
+                  expect(data.version.base).to.equal('v1');
+                  expect(data.version.sub).to.not.equal(0);
+                  done();
+                });
+          });
+        });
+  });
 });
